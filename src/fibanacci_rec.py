08d786081c2810157725312c8e238cf19da6c7b6
_cracker <chintan.pokhrel@gmail.com>
2017-12-18 07:22:14 +0530

    added exceptions to fibanacci_rec

--- src/fibanacci_rec.py
+++ src/fibanacci_rec.py
@@ -3,11 +3,22 @@ class BadFibanacci:
 		self.f = []
 	
 	def get(self, n):
-		if n==1 or n==0:
+		if n<=1:
 			return 1
 		return self.get(n-1) + self.get(n-2)
 	
 
-f = BadFibanacci()
+from sys import argv
 
-print f.get(150)
+try:
+	num = int(argv[1])
+	f = BadFibanacci()
+	print f.get(num)
+except ValueError:
+	print "I can't print fibonacci for non ints idiot."
+except IndexError:
+	print "Here's how you use this program: %s num" % argv[0]
+except Exception as e:
+	print "Something unexpected, here's what happened:"
+	print e
+
